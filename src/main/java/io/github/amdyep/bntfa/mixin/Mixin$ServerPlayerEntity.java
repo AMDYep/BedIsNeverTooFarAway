a00ef6d77805366bca34dfbe8760d1a62e4fa4fa
AMDYep <amdyep@outlook.com>
2024-01-18 21:12:18 +0800

    1.18.2 forge port

--- src/main/java/io/github/amdyep/bntfa/mixin/Mixin$ServerPlayerEntity.java
+++ src/main/java/io/github/amdyep/bntfa/mixin/Mixin$ServerPlayerEntity.java
@@ -1,14 +1,14 @@
 package io.github.amdyep.bntfa.mixin;
 
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerPlayerEntity.class)
+@Mixin(ServerPlayer.class)
 public class Mixin$ServerPlayerEntity {
     @Inject(
             method = "bedInRange",
